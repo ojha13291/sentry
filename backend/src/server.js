0b97e16766cebd69a1e6e7488dddc06b1741b426
AyushKumar675ui <kiliniko.ai.official@gmail.com>
2025-10-20 01:45:46 +0530

    final commit may be

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,10 +8,12 @@ import chatRoutes from './routes/chat.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT
 const app = express();
+const __dirname = path.resolve();
 app.use(cookieParser());
 app.use(express.json());
 
@@ -29,10 +31,15 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes); // user routes
 app.use('/api/chat', chatRoutes); // chat routes
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-});
+        console.log(`Server is running on port ${PORT}`);
+        connectDB();
+    });
